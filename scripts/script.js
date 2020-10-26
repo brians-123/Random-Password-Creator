954742d329114dff025eaab6194b8dd5267c0e07
brians-123 <brian_shor@hotmail.com>
2020-10-25 20:36:10 -0700

    mostly working but generating a NaN somewhere in the code

--- scripts/script.js
+++ scripts/script.js
@@ -44,10 +44,16 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
   //first we define the arrays that store our character options. 
-  var lowerArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var lowerArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+    + "n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var numberArray = ["0","1","2","3","4","5","6","7","8","9"];
-  var upperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  
+  var upperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+    + "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var specialCharArray = ["!","\"","#","$","%","\&","\'","(",")",
+    + "*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\",
+    + "]","^","_","`","{","|","}","~"];
+
+
   //this will store all of the values from the character options selected by the user
   var includeArray = [];
 
@@ -63,7 +69,37 @@ function generatePassword(){
   var  includeLowerCase = confirm("Do you want to use lowercase letters?");
   console.log("number of characters" + numOfCharacters);
   if(includeLowerCase){
-    includeArray.push(lowerArray);
+
+    for(i=0; i<lowerArray.length; i++){
+      includeArray.push(lowerArray[i]);
+    }
+  }
+
+  var  includeUpperCase = confirm("Do you want to use uppercase letters?");
+  console.log("number of characters" + numOfCharacters);
+  if(includeUpperCase){
+
+    for(i=0; i<upperArray.length; i++){
+      includeArray.push(upperArray[i]);
+    }
+  }
+
+  var  includeNumbers = confirm("Do you want to use numbers?");
+  console.log("number of characters" + numOfCharacters);
+  if(includeNumbers){
+
+    for(i=0; i<numberArray.length; i++){
+      includeArray.push(numberArray[i]);
+    }
+  }
+
+  var  includeSpecialChar = confirm("Do you want to use special characters?");
+  console.log("number of characters" + numOfCharacters);
+  if(includeSpecialChar){
+
+    for(i=0; i<specialCharArray.length; i++){
+      includeArray.push(specialCharArray[i]);
+    }
   }
 
   if(includeArray.length===0){
@@ -72,19 +108,21 @@ function generatePassword(){
   }
 
   console.log("includeArray values: " + includeArray);
+  console.log("includeArray position 0" + includeArray[0]);
+  console.log("lowerArray position 0" + lowerArray[0]);
   console.log(includeArray.length);
 
   function randomizeNumber(maxNumber){
     console.log("includeArray.length: " + includeArray.length)
     var foundNumber = Math.floor(Math.random() * includeArray.length);
     console.log("found number: " + foundNumber);
+    return foundNumber;
   }
 
   var myPassword = [];
 
-for(i=0; i<numOfCharacters.length; i++){
-  myPassword = myPassword.push(randomizeNumber(numOfCharacters));
-  console.log("here is my password after the rnadomize loop" + myPassword);
+for(i=0; i<numOfCharacters; i++){
+  myPassword.push(includeArray[(randomizeNumber(numOfCharacters))]);
 }
 
   return myPassword;
